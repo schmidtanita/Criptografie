7b47e03f0e1b249b343c2342d2098173e14c2f17
schmidtanita <schmidt.anita00@gmail.com>
2026-01-14 20:01:09 +0200

    New Wpf project - Functii Hash Criptografice

---
+++ Functii_Hash_Criptografice/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Functii_Hash_Criptografice;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
